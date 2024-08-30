532878857831bdcc8d1ed330d3c325927826a98b
Min01001 <ptunmt@gmail.com>
2024-08-30 12:15:35 +0630

    modified

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
   <div class="container" style="padding-top: 20px;">
     <nav class="navbar navbar-expand-lg ftco_navbar ftco-navbar-light" id="ftco-navbar">
       <div class="container-fluid custom-navbar">
-        <a class="navbar-brand" href="#">Watch</a>			  
+        <a class="navbar-brand" href="#">Watch Store</a>			  
         <div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="fa fa-bars"></span> Menu
@@ -10,12 +10,13 @@
           <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto mr-md-3">
               <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-              <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-              <li class="nav-item"><a href="#" class="nav-link">Work</a></li>
-              <li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
-              <li class="nav-item"><a href="new_product.php" class="nav-link">Contact</a></li>
+              <li class="nav-item"><a href="new_product.php" class="nav-link">New Product</a></li>
             </ul>
           </div>
+        </div>
+
+        <div>
+          <p><strong>Phone</strong><span> 09 777 444 222</span> <br> <strong>Facebook</strong><span> Account name</span> </p>
         </div>
       </div>
     </nav>
